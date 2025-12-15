58433cd150adbcb9d351fa27635db80c90b867a3
John-Isis-Bantog <bantogjohnisis@gmail.com>
2025-12-15 11:57:51 +0800

    migration is now correct

--- database/migrations/2025_12_15_032903_create_payments_table.php
+++ database/migrations/2025_12_15_032903_create_payments_table.php
@@ -11,11 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->decimal('grand_total', 10, 2);
-            $table->enum('status', ['pending', 'paid', 'cancelled', 'refunded']);
+            $table->foreignId('order_id')->constrained()->cascadeOnDelete();
+            $table->string('stripe_payment_intent_id')->unique();
+            $table->decimal('amount_paid', 10, 2);
+            $table->enum('status', ['pending', 'succeeded', 'failed', 'refunded']);
             $table->timestamps();
         });
     }
@@ -25,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('payments');
     }
 };
