84c4abbdf9288626dc46177c40ce92c0b012f6e4
John-Isis-Bantog <bantogjohnisis@gmail.com>
2025-12-15 14:50:19 +0800

    added controller, middleware, and routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -14,4 +16,7 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
+Route::middleware(['auth', IsAdmin::class])->group(function () {
+    Route::resource('admin', AdminController::class);
+});
+require __DIR__ . '/settings.php';
