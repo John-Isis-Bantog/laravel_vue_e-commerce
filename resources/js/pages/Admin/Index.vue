61bc943b4bbba5464b4567ea683e8a58fbd17c95
John-Isis-Bantog <bantogjohnisis@gmail.com>
2025-12-16 13:09:19 +0800

    added chartjs, item and catgory controller and placeholdeer for the admin/index

--- resources/js/pages/Admin/Index.vue
+++ resources/js/pages/Admin/Index.vue
@@ -3,6 +3,8 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
+import { Chart, PieController, ArcElement } from 'chart.js';
+import { ref, onMounted } from 'vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -10,6 +12,32 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: dashboard().url,
     },
 ];
+
+const ctxPie = ref<HTMLCanvasElement | null>(null);
+Chart.register(PieController, ArcElement)
+onMounted(() => {
+
+    new Chart(ctxPie.value!.getContext('2d')!, {
+        type: 'pie',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
+            datasets: [{
+                data: [12, 19, 3, 5, 2],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.7)',
+                    'rgba(54, 162, 235, 0.7)',
+                    'rgba(255, 206, 86, 0.7)',
+                    'rgba(75, 192, 192, 0.7)',
+                    'rgba(153, 102, 255, 0.7)'
+                ],
+                borderColor: '#fff',
+                borderWidth: 2
+            }]
+        }
+    });
+})
+
+
 </script>
 
 <template>
@@ -17,5 +45,15 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="Dashboard" />
     <AppLayout :breadcrumbs="breadcrumbs">
         <h1 class="text-center">Admin Dashboard</h1>
+        <div class="w-1/2 h-1/2 flex justify-center  mx-auto">
+            <canvas id="pieChart" ref="ctxPie"></canvas>
+        </div>
+
+        <div class="grid lg:grid-cols-4 md:grid-cols-2 gap-5 mx-auto my-2">
+            <div class="bg-blue-500 p-4 flex justify-center hover:bg-blue-600 rounded-lg">Categories</div>
+            <div class="bg-red-500 p-4 flex justify-center hover:bg-red-600 rounded-lg">Items</div>
+            <div class="bg-green-500 p-4 flex justify-center hover:bg-green-600 rounded-lg">Total Orders</div>
+            <div class="bg-cyan-500 p-4 flex justify-center hover:bg-cyan-600 rounded-lg">Revenue $102</div>
+        </div>
     </AppLayout>
 </template>
