498203538b6cb047c748caf38bb0b5f0ab3344bc
John-Isis-Bantog <bantogjohnisis@gmail.com>
2025-12-17 13:16:06 +0800

    renamed the create page to more readable and standard naming

--- resources/js/pages/Admin/Create.vue
+++ resources/js/pages/Admin/Create.vue
@@ -14,9 +14,9 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 const schema = yup.object({
-    adminEmail: yup.string().required().email(),
-    adminName: yup.string().required(),
-    adminPassword: yup.string().required().min(8).matches(/[a-z]/, 'Must contain a lowercase letter').matches(/[A-Z]/, 'Must contain an uppercase letter').matches(/\d/, 'Must contain a number').matches(/[^a-zA-Z0-9]/, 'Must contain a special character'),
+    email: yup.string().required().email(),
+    name: yup.string().required(),
+    password: yup.string().required().min(8).matches(/[a-z]/, 'Must contain a lowercase letter').matches(/[A-Z]/, 'Must contain an uppercase letter').matches(/\d/, 'Must contain a number').matches(/[^a-zA-Z0-9]/, 'Must contain a special character'),
 });
 const form = useForm({
     email: '',
@@ -25,10 +25,11 @@ const form = useForm({
 })
 function submitForm(values: any) {
 
-    form.name = values.adminName;
-    form.email = values.adminEmail;
-    form.password = values.adminPassword;
+    form.name = values.name;
+    form.email = values.email;
+    form.password = values.password;
     form.post(admin.store().url);
+
 
 }
 
@@ -41,22 +42,22 @@ function submitForm(values: any) {
     <AppLayout :breadcrumbs="breadcrumbs">
         <h1 class="text-center">Admin Create Page</h1>
         <Form :validation-schema="schema" class="w-1/2 mx-auto space-y-2" @submit="submitForm">
-            <Label for="adminName">Name</Label>
-            <Field type="text" name="adminName" :validate-on-input="true" placeholder="Enter Admin Name"
-                autocomplete="username"></Field>
-            <ErrorMessage class="text-red-500" name="adminName" />
+            <Label for="name">Name</Label>
+            <Field type="text" name="name" :validate-on-input="true" placeholder="Enter Admin Name" autocomplete="name">
+            </Field>
+            <ErrorMessage class="text-red-500" name="name" />
             <p v-if="form.errors.name">{{ form.errors.name }}</p>
-            <Label for="adminEmail">Email</Label>
-            <Field type="email" name="adminEmail" :validate-on-input="true" placeholder="Enter Admin Email"
+            <Label for="email">Email</Label>
+            <Field type="email" name="email" :validate-on-input="true" placeholder="Enter Admin Email"
                 autocomplete="email"></Field>
             <p v-if="form.errors.email">{{ form.errors.email }}</p>
-            <ErrorMessage class="text-red-500" name="adminEmail" />
-            <Label for="adminPassword">Password</Label>
-            <Field type="password" :validate-on-input="true" name="adminPassword" placeholder="Enter Password"
+            <ErrorMessage class="text-red-500" name="email" />
+            <Label for="password">Password</Label>
+            <Field type="password" :validate-on-input="true" name="password" placeholder="Enter Password"
                 autocomplete="current-password">
             </Field>
             <p v-if="form.errors.password">{{ form.errors.password }}</p>
-            <ErrorMessage class="text-red-500" name="adminPassword" />
+            <ErrorMessage class="text-red-500" name="password" />
             <div class="flex justify-center">
                 <Button type="submit">Submit</Button>
             </div>
