ba663a418e4d0ecabb86c5beafea22a92f6239f9
John-Isis-Bantog <bantogjohnisis@gmail.com>
2025-12-19 08:39:22 +0800

    feat(category): enhanced UI

--- resources/js/pages/Admin/Category/Create.vue
+++ resources/js/pages/Admin/Category/Create.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import Button from '@/components/ui/button/Button.vue';
 import Input from '@/components/ui/input/Input.vue';
 import Label from '@/components/ui/label/Label.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
@@ -20,16 +21,21 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="Admin Create" />
     <AppLayout :breadcrumbs="breadcrumbs">
         <h1 class="text-center">Admin Create Page</h1>
-        <Form>
+
+        <Form class="w-1/2 mx-auto space-y-3">
             <Label for="">Title</Label>
             <Input type="text" placeholder="Enter Title"></Input>
             <Label for="">Image</Label>
             <Input type="file" placeholder="Slect a file"></Input>
-            <Label for="">Featured</Label>
-            <Input type="checkbox"></Input>
-            <Label for="">Active</Label>
-            <Input type="checkbox"></Input>
-
+            <div class="flex">
+                <Label for="">Featured</Label>
+                <Input type="checkbox"></Input>
+                <Label for="">Active</Label>
+                <Input type="checkbox"></Input>
+            </div>
+            <Button type="submit" class="">Submit</Button>
         </Form>
+
+
     </AppLayout>
 </template>
