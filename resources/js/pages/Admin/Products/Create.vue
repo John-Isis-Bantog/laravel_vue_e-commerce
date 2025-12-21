ca075261229d7ec0cd5b309c932ffe9b49a5aa82
John-Isis-Bantog <bantogjohnisis@gmail.com>
2025-12-21 16:48:11 +0800

    refactor(product): add placeholder page for product create

--- resources/js/pages/Admin/Products/Create.vue
+++ resources/js/pages/Admin/Products/Create.vue
@@ -1,14 +1,24 @@
 <script lang="ts" setup>
+import Button from '@/components/ui/button/Button.vue';
+import Input from '@/components/ui/input/Input.vue';
+import Label from '@/components/ui/label/Label.vue';
+import Select from '@/components/ui/select/Select.vue';
+import SelectContent from '@/components/ui/select/SelectContent.vue';
+import SelectGroup from '@/components/ui/select/SelectGroup.vue';
+import SelectItem from '@/components/ui/select/SelectItem.vue';
+import SelectTrigger from '@/components/ui/select/SelectTrigger.vue';
+import SelectValue from '@/components/ui/select/SelectValue.vue';
+import Textarea from '@/components/ui/textarea/Textarea.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import productRoute from '@/routes/product';
 import { BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import { Form, Head, Link } from '@inertiajs/vue3';
 
 
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
-        title: 'Admin Category Index Page',
+        title: 'Admin Product Create Page',
         href: productRoute.index().url,
     },
 ];
@@ -16,7 +26,37 @@ const breadcrumbs: BreadcrumbItem[] = [
 
 <template>
 
-    <Head title="Category Create" />
+    <Head title="Product Create" />
     <AppLayout :breadcrumbs="breadcrumbs">
+        <Link :href="productRoute.index()"><Button>Back</Button></Link>
+        <div class="w-1/2 mx-auto">
+            <Form>
+                <Label for="name">Name</Label>
+                <Input type="text" name="name" placeholder="Name"></Input>
+
+                <Label for="description">Description</Label>
+                <Textarea name="description"></Textarea>
+
+                <Label for="price">Price</Label>
+                <Input type="number" min="1"></Input>
+
+                <Select>
+                    <SelectTrigger class="w-[180px]">
+                        <SelectValue placeholder="Active?" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectItem value="1">
+                                Yes
+                            </SelectItem>
+                            <SelectItem value="0">
+                                No
+                            </SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+                <Button type="submit">Add Product</Button>
+            </Form>
+        </div>
     </AppLayout>
 </template>
