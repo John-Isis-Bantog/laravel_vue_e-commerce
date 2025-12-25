b0c977c656d7fe7805d0cfefcc13b6a9e91da6fd
John-Isis-Bantog <bantogjohnisis@gmail.com>
2025-12-25 23:14:13 +0800

    refactor(user): add placeholder page

---
+++ resources/js/pages/User/Edit.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import AppLayout from '@/layouts/AppLayout.vue';
+import user from '@/routes/user';
+import { BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Admin Create Page',
+        href: user.index().url,
+    },
+];
+</script>
+
+<template>
+
+    <Head title="Admin Create" />
+    <AppLayout :breadcrumbs="breadcrumbs">
+
+    </AppLayout>
+</template>
