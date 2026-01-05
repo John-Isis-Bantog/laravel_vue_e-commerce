43eaa7df71f7ccc80034038e1d620fbfb8ee160c
John-Isis-Bantog <bantogjohnisis@gmail.com>
2026-01-05 15:28:31 +0800

    refactor(checkout): can now output the data

--- resources/js/pages/User/Checkout/Index.vue
+++ resources/js/pages/User/Checkout/Index.vue
@@ -1,4 +1,10 @@
 <script lang="ts" setup>
+import Card from '@/components/ui/card/Card.vue';
+import CardContent from '@/components/ui/card/CardContent.vue';
+import CardDescription from '@/components/ui/card/CardDescription.vue';
+import CardHeader from '@/components/ui/card/CardHeader.vue';
+import CardTitle from '@/components/ui/card/CardTitle.vue';
+import Input from '@/components/ui/input/Input.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import user from '@/routes/user';
 import { BreadcrumbItem } from '@/types';
@@ -10,6 +16,22 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+interface SelectedItem {
+    id: number,
+    product_id: number,
+    quantity: number,
+    is_selected: boolean,
+    product: {
+        name: string,
+        description: string,
+        price: number,
+        image: string
+    }
+}
+
+const props = defineProps<{
+    selectedItems: SelectedItem[]
+}>()
 </script>
 
 
@@ -17,5 +39,29 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <Head title="Admin Create" />
     <AppLayout :breadcrumbs="breadcrumbs">
+        <Card v-for="selectedItem in selectedItems" class="w-full max-w-sm" :key="selectedItem.id">
+            <!-- <Link :href="user.show(cartItem.id)"> -->
+            <CardHeader>
+
+                <img v-if="selectedItem.product.image" :src="selectedItem.product.image" alt=""><span v-else><img
+                        src="https://hsaubfbdbzpjgwazahvz.supabase.co/storage/v1/object/public/laravel_vue_e_commerce_bucket/public/image_not_available.jpg"
+                        alt=""></span>
+                <CardTitle>{{ selectedItem.product.name }}</CardTitle>
+                <CardDescription>
+                    {{ selectedItem.product.description }}
+                </CardDescription>
+            </CardHeader>
+            <CardContent class="text-center flex justify-between">
+                <div class="">${{ selectedItem.product.price }}</div>
+                <div class="flex space-x-2 justify-around">
+                    <div class="flex w-15">
+                        <Input type="number" min="0" max="5" v-model="selectedItem.quantity"></Input>
+                    </div>
+                </div>
+
+            </CardContent>
+
+
+        </Card>
     </AppLayout>
 </template>
