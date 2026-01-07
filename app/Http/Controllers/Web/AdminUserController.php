1915a937dfe94284d0d925b1ee519d7e72305550
John-Isis-Bantog <bantogjohnisis@gmail.com>
2026-01-07 12:52:48 +0800

    refactor(adminusercontroller): get customers from the database

--- app/Http/Controllers/Web/AdminUserController.php
+++ app/Http/Controllers/Web/AdminUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -13,6 +14,7 @@ class AdminUserController extends Controller
      */
     public function index()
     {
+        $users = User::where('role', 'customer')->get();
         return Inertia::render('Admin/User/Index');
     }
 
