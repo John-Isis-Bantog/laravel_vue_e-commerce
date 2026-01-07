3901baa8b5bd83dc346f5a161f1fdfeb9efb2774
John-Isis-Bantog <bantogjohnisis@gmail.com>
2026-01-07 13:02:59 +0800

    refactor(adminusercontroller):can now display all the data

--- resources/js/pages/Admin/User/Index.vue
+++ resources/js/pages/Admin/User/Index.vue
@@ -1,8 +1,15 @@
 <script lang="ts" setup>
+import Button from '@/components/ui/button/Button.vue';
+import Table from '@/components/ui/table/Table.vue';
+import TableBody from '@/components/ui/table/TableBody.vue';
+import TableCell from '@/components/ui/table/TableCell.vue';
+import TableHead from '@/components/ui/table/TableHead.vue';
+import TableHeader from '@/components/ui/table/TableHeader.vue';
+import TableRow from '@/components/ui/table/TableRow.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import products from '@/routes/products';
 import { BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -10,6 +17,16 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: products.index().url,
     },
 ];
+
+interface User {
+    id: number,
+    name: string,
+    email: string,
+}
+
+const props = defineProps<{
+    user: User[]
+}>()
 </script>
 
 
@@ -17,5 +34,26 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <Head title="Admin Create" />
     <AppLayout :breadcrumbs="breadcrumbs">
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>ID</TableHead>
+                    <TableHead>Name</TableHead>
+                    <TableHead>Email</TableHead>
+                    <TableHead>Actions</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableRow v-for="user in props.user" :key="user.id">
+                    <TableCell>{{ user.id }}</TableCell>
+                    <TableCell>{{ user.name }}</TableCell>
+                    <TableCell>{{ user.email }}</TableCell>
+                    <TableCell class="gap-2 flex">
+                        <Link> <Button variant="primary">Edit</Button></Link>
+                        <Link> <Button variant="destructive">Delete</Button></Link>
+                    </TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
     </AppLayout>
 </template>
