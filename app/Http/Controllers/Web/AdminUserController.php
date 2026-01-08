d4899f3c855c5d01dccb2aa5b49ee32c5cb847f1
John-Isis-Bantog <bantogjohnisis@gmail.com>
2026-01-08 10:59:55 +0800

    refactor(adminusercontroller):can nowget the data from the database

--- app/Http/Controllers/Web/AdminUserController.php
+++ app/Http/Controllers/Web/AdminUserController.php
@@ -17,50 +17,16 @@ class AdminUserController extends Controller
         $user = User::where('role', 'customer')->get();
         return Inertia::render('Admin/User/Index', ['user' => $user]);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        $user = User::where('role', 'customer')->findOrFail($id);
+        return Inertia::render('Admin/User/Edit', ['user' => $user]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
