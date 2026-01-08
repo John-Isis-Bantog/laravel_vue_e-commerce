86385c072b3092b635a52edf4ea20beaa0bb2b6d
John-Isis-Bantog <bantogjohnisis@gmail.com>
2026-01-08 11:01:19 +0800

    refator(user): add placeholder

--- resources/js/pages/Admin/User/Edit.vue
+++ resources/js/pages/Admin/User/Edit.vue
@@ -1,8 +1,12 @@
 <script lang="ts" setup>
+import Button from '@/components/ui/button/Button.vue';
+import Input from '@/components/ui/input/Input.vue';
+import Label from '@/components/ui/label/Label.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import products from '@/routes/products';
+import userRoute from '@/routes/Users';
 import { BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import { Form, Head, Link, useForm } from '@inertiajs/vue3';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -10,6 +14,22 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: products.index().url,
     },
 ];
+
+interface User {
+    id: number,
+    name: string,
+    email: string,
+}
+const props = defineProps<{
+    user: User
+}>()
+
+const form = useForm({
+    name: props.user.name,
+    email: props.user.email
+})
+
+
 </script>
 
 
@@ -17,5 +37,21 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <Head title="Admin Create" />
     <AppLayout :breadcrumbs="breadcrumbs">
+        <Link :href="userRoute.index.url()"><Button variant="outline">Back</Button></Link>
+        <Form class="space-y-2 w-1/2 mx-auto">
+            <div class="">
+                <Label for="name">Name</Label>
+                <Input type="text"></Input>
+            </div>
+
+            <div class="">
+                <Label for="email">Email</Label>
+                <Input type="email"></Input>
+            </div>
+            <div class="flex justify-center">
+                <Button variant="primary">Submit</Button>
+            </div>
+
+        </Form>
     </AppLayout>
 </template>
