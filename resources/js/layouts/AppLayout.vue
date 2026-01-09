388d69276daf301cdc2481e43dea6b0f3256537d
John-Isis-Bantog <bantogjohnisis@gmail.com>
2026-01-09 18:13:35 +0800

    refactor(applayout): will shoow the message if sucess or failed

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -4,6 +4,9 @@ import AlertDescription from '@/components/ui/alert/AlertDescription.vue';
 import AlertTitle from '@/components/ui/alert/AlertTitle.vue';
 import AppLayout from '@/layouts/app/AppHeaderLayout.vue';
 import type { BreadcrumbItemType } from '@/types';
+import { ref, onMounted, computed } from 'vue';
+import { usePage } from '@inertiajs/vue3';
+
 
 interface Props {
     breadcrumbs?: BreadcrumbItemType[];
@@ -12,17 +15,33 @@ interface Props {
 withDefaults(defineProps<Props>(), {
     breadcrumbs: () => [],
 });
+const page = usePage();
+const successMessage = ref('');
+
+onMounted(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get('success') === 'true') {
+        successMessage.value = 'Product has been successfully bought!';
+    }
+});
+const alertMessage = computed(() => {
+    if (page.props.flash?.error) return page.props.flash.error;
+    if (page.props.flash?.success) return page.props.flash.success;
+    if (successMessage.value) return successMessage.value;
+    return '';
+});
+
 </script>
 
 <template>
     <AppLayout :breadcrumbs="breadcrumbs">
-        <Alert class=" mx-auto w-1/2 top-5 sticky z-50" v-if="$page.props.flash?.success || $page.props.flash?.error">
+        <Alert v-if="alertMessage" class="mx-auto w-1/2 top-5 sticky z-50">
             <AlertTitle>Info</AlertTitle>
             <AlertDescription>
-                {{ $page.props.flash.success }}
-                {{ $page.props.flash.error }}
+                {{ alertMessage }}
             </AlertDescription>
         </Alert>
+
         <slot />
     </AppLayout>
 </template>
