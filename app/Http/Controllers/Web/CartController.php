bb655645ca9c34d676f16c178db2d859a63d6c61
John-Isis-Bantog <bantogjohnisis@gmail.com>
2026-01-10 12:26:14 +0800

    refactor(addtocart): stopping >5 quantity and better readability

--- app/Http/Controllers/Web/CartController.php
+++ app/Http/Controllers/Web/CartController.php
@@ -28,21 +28,16 @@ class CartController extends Controller
             'quantity' => 'required|integer|min:1|max:5'
         ]);
         $user = auth()->user();
-        $product = Product::findOrFail($validated['id']);
-        $cartItem = $user->cartItems()->where('product_id', $product->id)->first();
-
-        if ($cartItem) {
-            $cartItem->update([
-                'quantity' => $cartItem->quantity + $validated['quantity']
-            ]);
-        } else {
-            $user->cartItems()->create([
-                'product_id' => $product->id,
-                'quantity' => $validated['quantity']
-            ]);
+        $productId = Product::findOrFail($validated['id']);
+        $cartItem = $user->cartItems()->where('product_id', $productId->id)->first();
+        $newQty = ($cartItem->quantity ?? 0) + 1;
+        if ($newQty > 5) {
+            return back()->with('error', 'Quantity is Greater than 5. Cannot add more Quantity');
         }
-
-
+        $user->cartItems()->updateOrCreate(
+            ['product_id' => $productId->id],
+            ['quantity' => $newQty]
+        );
         return back()->with('success', 'Added to Cart');
     }
 
