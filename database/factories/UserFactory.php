6d9bc8326dff841bcc199d9551d2958f983a4349
John-Isis-Bantog <bantogjohnisis@gmail.com>
2026-01-12 15:22:10 +0800

    refactor(userfactory): add admin and customer state

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -40,7 +40,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
@@ -50,10 +50,19 @@ class UserFactory extends Factory
      */
     public function withoutTwoFactor(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'two_factor_confirmed_at' => null,
         ]);
+    }
+    public function customer()
+    {
+        return $this->state(fn() => ['role' => 'customer']);
+    }
+
+    public function admin()
+    {
+        return $this->state(fn() => ['role' => 'admin']);
     }
 }
