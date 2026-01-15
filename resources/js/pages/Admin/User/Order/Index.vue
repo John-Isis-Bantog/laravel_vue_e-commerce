5c2fdbd00373bf9d2df2b932109c74c887da8906
John-Isis-Bantog <bantogjohnisis@gmail.com>
2026-01-15 11:17:48 +0800

    feat(order index): add index placeholder

---
+++ resources/js/pages/Admin/User/Order/Index.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import AppLayout from '@/layouts/AppLayout.vue';
+import { dashboard } from '@/routes';
+import { BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'User',
+        href: dashboard().url,
+    },
+];
+
+</script>
+
+<template>
+
+    <Head title="Dashboard" />
+    <AppLayout :breadcrumbs="breadcrumbs">
+    </AppLayout>
+</template>
