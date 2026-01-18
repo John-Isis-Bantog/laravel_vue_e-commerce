24264a5c72d289595fc5473ea27d162494763fde
John-Isis-Bantog <bantogjohnisis@gmail.com>
2026-01-18 12:17:59 +0800

    refactor(order index): can now output price and quantity

--- resources/js/pages/Admin/User/Order/Index.vue
+++ resources/js/pages/Admin/User/Order/Index.vue
@@ -27,7 +27,7 @@ interface Product {
     image: string | null
     price: number
 }
-interface OrderItem {
+interface order_item {
     id: number
     quantity: number
     price: number
@@ -37,13 +37,12 @@ interface Order {
     id: number
     grand_total: number
     status: 'pending' | 'paid' | 'cancelled' | 'refunded'
-    orderItems: OrderItem[]
+    order_items: order_item[]
     createdAt: string
 }
 
 const props = defineProps<{
     orders: Order[];
-    orderItems: OrderItem[];
 }>() 
 </script>
 
@@ -52,37 +51,41 @@ const props = defineProps<{
     <Head title="Dashboard" />
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex flex-col">
-            <Card v-for="order in props.orders">
+            <Card v-for="order in props.orders" :key="order.id">
+                <div class="" v-for="item in order.order_items" :key="item.id">
 
-                <!-- <Link :href="user.show(cartItem.id)"> -->
-                <CardHeader class="flex justify-between">
-                    <div class="flex space-x-2">
-                        <img class="max-w-3xs" alt=""><span><img class="max-w-3xs"
-                                src="https://hsaubfbdbzpjgwazahvz.supabase.co/storage/v1/object/public/laravel_vue_e_commerce_bucket/public/image_not_available.jpg"
-                                alt=""></span>
-                        <div class="">
-                            <CardTitle>{{ }}</CardTitle>
-                            <CardDescription>
-                                {{ }}
-                            </CardDescription>
-                        </div>
-                    </div>
-                    <CardContent class="  ">
-                        <div class="">₱{{ }}</div>
-                        <div class="space-2 ">
-                            <div class=" ">
-                                <div class="">
-                                    <Label>Quantity:{{ }} </Label>
-                                    <!-- <Input type="number" min="0" max="5"></Input> -->
-                                </div>
-                                <div class="">
-                                    <Label>Status:{{ order.status }} </Label>
-                                </div>
+                    <!-- <Link :href="user.show(cartItem.id)"> -->
+                    <CardHeader class="flex justify-between">
+                        <div class="flex space-x-2">
+                            <img class="max-w-3xs" alt=""><span><img class="max-w-3xs"
+                                    src="https://hsaubfbdbzpjgwazahvz.supabase.co/storage/v1/object/public/laravel_vue_e_commerce_bucket/public/image_not_available.jpg"
+                                    alt=""></span>
+                            <div class="">
+                                <CardTitle>{{ }}</CardTitle>
+                                <CardDescription>
+                                    {{ }}
+                                </CardDescription>
                             </div>
                         </div>
-                    </CardContent>
-                </CardHeader>
 
+                        <CardContent class="  ">
+                            <div class="">₱{{ item.price }}</div>
+                            <div class="space-2 ">
+                                <div class=" ">
+                                    <div class="">
+                                        <Label>Quantity:{{ item.quantity }} </Label>
+                                        <!-- <Input type="number" min="0" max="5"></Input> -->
+                                    </div>
+                                    <div class="">
+                                        <!-- <Label>Status:{{ order.status }} </Label> -->
+                                    </div>
+                                </div>
+                            </div>
+                        </CardContent>
+
+
+                    </CardHeader>
+                </div>
             </Card>
         </div>
     </AppLayout>
