980a4384512d165abfc280c1ca77a1f71238ee7d
John-Isis-Bantog <bantogjohnisis@gmail.com>
2026-01-20 14:41:08 +0800

    refactor(route): add address endpoint

---
+++ resources/js/pages/settings/Addresses.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import AppLayout from '@/layouts/AppLayout.vue';
+import { edit } from '@/routes/profile';
+import { BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+import SettingsLayout from '@/layouts/settings/Layout.vue';
+
+const breadcrumbItems: BreadcrumbItem[] = [
+    {
+        title: 'Profile settings',
+        href: edit().url,
+    },
+];
+</script>
+
+<template>
+    <AppLayout :breadcrumbs="breadcrumbItems">
+
+        <Head title="Profile settings" />
+        <SettingsLayout>
+            <h1>hello world</h1>
+        </SettingsLayout>
+    </AppLayout>
+
+</template>
--- routes/settings.php
+++ routes/settings.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Settings\AddressesController;
 use App\Http\Controllers\Settings\PasswordController;
 use App\Http\Controllers\Settings\ProfileController;
 use App\Http\Controllers\Settings\TwoFactorAuthenticationController;
@@ -25,4 +26,6 @@ Route::middleware('auth')->group(function () {
 
     Route::get('settings/two-factor', [TwoFactorAuthenticationController::class, 'show'])
         ->name('two-factor.show');
+
+    Route::get('settings/addresses', [AddressesController::class, 'edit'])->name('addresses.edit');
 });
