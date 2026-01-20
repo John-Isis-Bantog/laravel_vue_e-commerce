1e79920e18b34309ded0eb77e7491155f0eadd0b
John-Isis-Bantog <bantogjohnisis@gmail.com>
2026-01-20 14:40:36 +0800

    feat(addressescontroller): add adress controller

---
+++ app/Http/Controllers/Settings/AddressesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Settings;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class AddressesController extends Controller
+{
+
+    public function edit()
+    {
+        return Inertia::render('settings/Addresses');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+}
