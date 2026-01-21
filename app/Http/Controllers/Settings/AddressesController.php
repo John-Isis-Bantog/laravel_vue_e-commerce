1d542dd23f1f81f5ade34b278b1b1b47f1bb06a2
John-Isis-Bantog <bantogjohnisis@gmail.com>
2026-01-21 09:16:13 +0800

    refactor(addressescontroller): add validation

--- app/Http/Controllers/Settings/AddressesController.php
+++ app/Http/Controllers/Settings/AddressesController.php
@@ -23,7 +23,17 @@ class AddressesController extends Controller
         // dd($request);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'recipient_name' => 'required|string|max:255',
+            'phoneNumber' => 'required|numeric',
+            'city' => 'required|string|max:255',
+            'address_line_1' => 'required|string|max:255',
+            'address_line_2' => 'nullable|string|max:255',
+            'province' => 'required|string|max:255',
+            'postal_code' => 'required|numeric',
+        ]);
+        dd($validatedData);
+    }
 }
