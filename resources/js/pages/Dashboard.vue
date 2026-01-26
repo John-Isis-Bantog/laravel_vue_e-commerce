ceca6f369d6ef137bb8ce3d0a9fa172d26e513b7
John-Isis-Bantog <bantogjohnisis@gmail.com>
2026-01-26 17:54:19 +0800

    refactor(dashboard): better styling

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -54,44 +54,48 @@ const canGoNext = computed(() => {
 
     <Head title="Dashboard" />
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="flex overflow-hidden gap-5 my-4 flex-nowrap relative">
-            <div class="absolute top-1/3"><Button v-if="seenCategories > 0" @click="seenCategories--">
-                    <MoveLeft />
+        <div class="my-4">
+            <div class="flex overflow-hidden gap-5 flex-nowrap relative mx-2">
+                <div class="absolute top-1/3 hover:opacity-60 duration-500 ease-out"><Button v-if="seenCategories > 0"
+                        @click="seenCategories--">
+                        <MoveLeft />
+                    </Button>
+                </div>
+                <Link :href="categoryProduct(category.id).url" v-for="category in visibleCategories" :key="category.id"
+                    class="w-1/5">
+                    <Card class="w-full max-w-sm ">
+                        <CardHeader>
+                            <img v-if="category.image" :src=category.image alt="">
+                            <span v-else><img
+                                    src="https://hsaubfbdbzpjgwazahvz.supabase.co/storage/v1/object/public/laravel_vue_e_commerce_bucket/public/image_not_available.jpg"
+                                    alt=""></span>
+                            <CardTitle class="text-center"> {{ category.title }}</CardTitle>
+                        </CardHeader>
+                    </Card>
+                </Link>
+                <Button class="absolute top-1/3 right-0 hover:opacity-60 duration-500 ease-out" v-if="canGoNext"
+                    @click="seenCategories++">
+                    <MoveRight />
                 </Button>
             </div>
-            <Link :href="categoryProduct(category.id).url" v-for="category in visibleCategories" :key="category.id"
-                class="w-1/5">
-                <Card class="w-full max-w-sm">
-                    <CardHeader>
-                        <img v-if="category.image" :src=category.image alt="">
-                        <span v-else><img
-                                src="https://hsaubfbdbzpjgwazahvz.supabase.co/storage/v1/object/public/laravel_vue_e_commerce_bucket/public/image_not_available.jpg"
-                                alt=""></span>
-                        <CardTitle class="text-center"> {{ category.title }}</CardTitle>
-                    </CardHeader>
-                </Card>
-            </Link>
-            <Button class="absolute top-1/3 right-0" v-if="canGoNext" @click="seenCategories++">
-                <MoveRight />
-            </Button>
-        </div>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-            <Link :href="user.show(product.id)" v-for="product in props.products" :key="product.id">
-                <Card class="w-full max-w-sm">
-                    <CardHeader>
-                        <img v-if="product.image" :src="product.image" alt=""><span v-else><img
-                                src="https://hsaubfbdbzpjgwazahvz.supabase.co/storage/v1/object/public/laravel_vue_e_commerce_bucket/public/image_not_available.jpg"
-                                alt=""></span>
-                        <CardTitle>{{ product.name }}</CardTitle>
-                        <!-- <CardDescription>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+                <Link :href="user.show(product.id)" v-for="product in props.products" :key="product.id">
+                    <Card class="w-full max-w-sm">
+                        <CardHeader>
+                            <img v-if="product.image" :src="product.image" alt=""><span v-else><img
+                                    src="https://hsaubfbdbzpjgwazahvz.supabase.co/storage/v1/object/public/laravel_vue_e_commerce_bucket/public/image_not_available.jpg"
+                                    alt=""></span>
+                            <CardTitle>{{ product.name }}</CardTitle>
+                            <!-- <CardDescription>
                             {{ product.description }}
                         </CardDescription> -->
-                    </CardHeader>
-                    <CardContent class="text-center">
-                        ₱{{ product.price }}
-                    </CardContent>
-                </Card>
-            </Link>
+                        </CardHeader>
+                        <CardContent class="text-center">
+                            ₱{{ product.price }}
+                        </CardContent>
+                    </Card>
+                </Link>
+            </div>
         </div>
     </AppLayout>
 </template>
